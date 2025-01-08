61c35958d365ee06f2435622898ec96f7fd1ea00
AmirHosein Nematzadeh <amir2symbian@gmail.com>
2025-01-09 02:35:34 +0330

    Delete poweradmin/www directory

--- poweradmin/www/sql/poweradmin-mysql-update-to-3.9.0.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE zone_templ_records MODIFY COLUMN content varchar(2048) NOT NULL;
--- poweradmin/www/sql/poweradmin-pgsql-update-to-3.9.0.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE zone_templ_records ALTER COLUMN content TYPE varchar(2048), ALTER COLUMN content SET NOT NULL;
